95287932544e7d5d544cbf5d979eb7cf278793bb
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-27 15:45:11 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,17 +1,19 @@
 <?php
 include 'db.php';
-include 'menu.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
-    if (mysqli_query($conn, $sql)) {
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+    if (mysqli_stmt_execute($stmt)) {
         echo "User registered successfully";
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        echo "Error: " . mysqli_stmt_error($stmt);
     }
+    mysqli_stmt_close($stmt);
     mysqli_close($conn);
 }
 ?>
