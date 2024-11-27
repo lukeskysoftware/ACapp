a42000cf744cbbf5eb4b0a5f0938b6fd5557bc96
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-27 14:30:38 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database";
+$username = "root";
+$password = "";
+$dbname = "your_database_name";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
