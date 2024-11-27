326b2f2a1873482097401fb09a8ab20b9b8dd3ce
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-27 17:04:15 +0100

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username = ?";
+    $sql = "SELECT * FROM cp_users WHERE username = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "s", $username);
     mysqli_stmt_execute($stmt);
