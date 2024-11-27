787482d0243052789b4060229cc2b011bda58ccf
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-27 14:28:47 +0100

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+}
+?>
+
+<nav>
+    <ul>
+        <li><a href="create_zone_form.html">Create Zone</a></li>
+        <li><a href="manage_appointments.php">Manage Appointments</a></li>
+        <li><a href="book_appointment_form.html">Book Appointment</a></li>
+        <li><a href="view_appointments.php">View Appointments</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</nav>
