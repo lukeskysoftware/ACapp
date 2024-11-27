738609fc3a5b95c1fda7dc0249f673a1b5a3cf2b
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-27 14:26:47 +0100

    Create register.php

---
+++ register.php
@@ -0,0 +1,33 @@
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+    if (mysqli_query($conn, $sql)) {
+        echo "User registered successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Registration</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form method="post" action="register.php">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
