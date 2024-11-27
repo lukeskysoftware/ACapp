1d0a3a1253f9f4fcb252077ba21a9c402c310341
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-27 09:51:39 +0100

    Update create_zone.php

--- create_zone.php
+++ create_zone.php
@@ -1,1 +1,18 @@
 
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $radius_km = $_POST['radius_km'];
+
+    $sql = "INSERT INTO zones (name, address, radius_km) VALUES ('$name', '$address', '$radius_km')";
+    if (mysqli_query($conn, $sql)) {
+        echo "New zone created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+?>
