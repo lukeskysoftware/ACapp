03059660bdc619cb78e82767a2ce87950ef49e99
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-27 19:24:32 +0100

    Update get_zone_data.php

--- get_zone_data.php
+++ get_zone_data.php
@@ -3,14 +3,31 @@ include 'db.php';
 
 if (isset($_GET['zone_id'])) {
     $zone_id = $_GET['zone_id'];
-    $sql = "SELECT * FROM cp_zones WHERE id = '$zone_id'";
-    $result = mysqli_query($conn, $sql);
+    
+    // Fetch zone data
+    $zoneSql = "SELECT * FROM cp_zones WHERE id = '$zone_id'";
+    $zoneResult = mysqli_query($conn, $zoneSql);
 
-    if ($result) {
-        $zone = mysqli_fetch_assoc($result);
+    if ($zoneResult) {
+        $zone = mysqli_fetch_assoc($zoneResult);
+
+        // Fetch slots data
+        $slotsSql = "SELECT day, time FROM cp_slots WHERE zone_id = '$zone_id'";
+        $slotsResult = mysqli_query($conn, $slotsSql);
+
+        if ($slotsResult) {
+            $slots = [];
+            while ($row = mysqli_fetch_assoc($slotsResult)) {
+                $slots[] = $row;
+            }
+            $zone['slots'] = $slots;
+        } else {
+            $zone['slots'] = [];
+        }
+
         echo json_encode($zone);
     } else {
-        echo json_encode(['error' => 'Error fetching data']);
+        echo json_encode(['error' => 'Error fetching zone data']);
     }
 
     mysqli_close($conn);
