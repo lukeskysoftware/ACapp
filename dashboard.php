5754e6cd1ecedc12368ff006247b0e15336c1a6a
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-27 14:29:40 +0100

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<?php
+include 'menu.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <h2>Welcome to the Dashboard</h2>
+    <p>Select an option from the menu.</p>
+</body>
+</html>
