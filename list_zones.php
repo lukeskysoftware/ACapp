f4b29187f762d2b6b354b3c7cc1bd54c2a823490
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-27 19:03:20 +0100

    Create list_zones.php

---
+++ list_zones.php
@@ -0,0 +1,34 @@
+<?php
+include 'db.php';
+
+$sql = "SELECT * FROM cp_zones";
+$result = mysqli_query($conn, $sql);
+
+echo "<h2>Gestione delle Zone</h2>";
+echo "<table border='1'>
+<tr>
+<th>Nome della Zona</th>
+<th>Indirizzo</th>
+<th>Azioni</th>
+</tr>";
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['address'] . "</td>";
+    echo "<td>
+            <form action='edit_zone_form.html' method='get' style='display:inline-block;'>
+                <input type='hidden' name='zone_id' value='" . $row['id'] . "'>
+                <button type='submit'>Modifica</button>
+            </form>
+            <form action='delete_zone.php' method='post' style='display:inline-block;' onsubmit='return confirm(\"Sei sicuro di voler eliminare questa zona?\");'>
+                <input type='hidden' name='zone_id' value='" . $row['id'] . "'>
+                <button type='submit'>Elimina</button>
+            </form>
+          </td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($conn);
+?>
