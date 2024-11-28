bb4c223397a1bcf673cea9779fb53d42955db38b
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-28 17:09:58 +0100

    Update address_calculate.php

--- address_calculate.php
+++ address_calculate.php
@@ -1,6 +1,10 @@
 <?php
 include 'db.php';
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include 'db.php';
+
 // Function to get API key from the database
 function getAPIKey() {
     global $conn;
