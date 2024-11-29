1f33ee8dc4c2e63df50a5a4e58ad2cd1cc80741e
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-29 22:43:22 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,14 +4,16 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
 }
 ?>
-
+<div class="pure-menu pure-menu-horizontal">
 <nav>
-    <ul>
-        <li><a href="create_zone_form.html">Create Zone</a></li>
-        <li><a href="list_zones.php">Manage Zones</a></li> <!-- Updated to point to list_zones.php -->
-        <li><a href="manage_appointments.php">Manage Appointments</a></li>
-        <li><a href="book_appointment_form.html">Book Appointment</a></li>
-        <li><a href="view_appointments.php">View Appointments</a></li>
-        <li><a href="logout.php">Logout</a></li>
+    <ul class="pure-menu-list">
+        <li class="pure-menu-item"><a class="pure-menu-link" href="create_zone_form.html">Crea Zona</a></li>
+        <li class="pure-menu-item"><a class="pure-menu-link" href="list_zones.php">Gestisci Zone</a></li> <!-- Updated to point to list_zones.php -->
+        <li class="pure-menu-item"><a class="pure-menu-link" href="book_appointment_form.php">Prenota Appuntamento</a></li>
+        <li class="pure-menu-item"><a class="pure-menu-link" href="manage_appointments.php">Gestisci Appuntamenti</a></li>
+        
+        <li class="pure-menu-item"><a class="pure-menu-link" href="view_appointments.php">Vedi Appuntamenti</a></li>
+        <li class="pure-menu-item"><a class="pure-menu-link" href="logout.php">Logout</a></li>
     </ul>
 </nav>
+</div>
