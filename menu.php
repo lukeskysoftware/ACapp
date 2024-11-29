056c8eeec31a4d1df1f64f8b3623e8e141147aee
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-29 22:44:08 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['user_id'])) {
     <ul class="pure-menu-list">
         <li class="pure-menu-item"><a class="pure-menu-link" href="create_zone_form.html">Crea Zona</a></li>
         <li class="pure-menu-item"><a class="pure-menu-link" href="list_zones.php">Gestisci Zone</a></li> <!-- Updated to point to list_zones.php -->
-        <li class="pure-menu-item"><a class="pure-menu-link" href="book_appointment_form.php">Prenota Appuntamento</a></li>
+        <li class="pure-menu-item"><a class="pure-menu-link" href="address_calculate.html">Prenota Appuntamento</a></li>
         <li class="pure-menu-item"><a class="pure-menu-link" href="manage_appointments.php">Gestisci Appuntamenti</a></li>
         
         <li class="pure-menu-item"><a class="pure-menu-link" href="view_appointments.php">Vedi Appuntamenti</a></li>
