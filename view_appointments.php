f4bf8603a5ea4e9f2495b282cc600d65429181bb
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-29 17:45:26 +0100

    Update view_appointments.php

--- view_appointments.php
+++ view_appointments.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Visualizza Appuntamenti</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
     <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'></script>
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
