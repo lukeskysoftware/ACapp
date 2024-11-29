2a72ab65329f3886efb43718371a8a5e4ac3eaae
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-29 17:44:23 +0100

    Update manage_patients.php

--- manage_patients.php
+++ manage_patients.php
@@ -69,6 +69,7 @@ $patients = getPatients($search);
 <html>
 <head>
     <title>Manage Patients</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
     <style>
         .modifica-btn {
             background-color: green;
