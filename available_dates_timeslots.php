417672bed28a47e5ba74e68881d4e9fc7cdb38cb
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-29 21:17:42 +0100

    Update available_dates_timeslots.php

--- available_dates_timeslots.php
+++ available_dates_timeslots.php
@@ -4,7 +4,14 @@ include 'db.php';
 $zone = isset($_GET['zone']) ? $_GET['zone'] : '';
 
 if ($zone) {
-    echo json_encode(getAvailableDatesAndTimeslots($zone));
+    try {
+        $data = getAvailableDatesAndTimeslots($zone);
+        echo json_encode($data);
+    } catch (Exception $e) {
+        error_log('Error fetching dates and timeslots: ' . $e->getMessage());
+        http_response_code(500);
+        echo json_encode(['error' => 'Internal Server Error']);
+    }
 } else {
     echo json_encode([]);
 }
