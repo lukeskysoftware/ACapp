0687a451fbdf09be4a6089fc3d72ccf7e2223bbb
lukeskysoftware <lukeskysoftware@gmail.com>
2024-11-29 21:22:04 +0100

    Update available_dates_timeslots.php

--- available_dates_timeslots.php
+++ available_dates_timeslots.php
@@ -1,6 +1,23 @@
 <?php
 include 'db.php';
 
+function getAvailableDatesAndTimeslots($zone) {
+    global $conn;
+    $datesAndTimeslots = [];
+    $sql = "SELECT DISTINCT appointment_date, appointment_time 
+            FROM cp_appointments 
+            WHERE zone_id = (SELECT id FROM cp_zones WHERE name = '" . mysqli_real_escape_string($conn, $zone) . "') 
+            AND appointment_date >= CURDATE() 
+            ORDER BY appointment_date, appointment_time ASC";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $datesAndTimeslots[$row['appointment_date']][] = $row['appointment_time'];
+        }
+    }
+    return $datesAndTimeslots;
+}
+
 $zone = isset($_GET['zone']) ? $_GET['zone'] : '';
 
 if ($zone) {
