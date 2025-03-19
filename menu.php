6690295fce9619497e8e11a40130faa929e2f46f
lukeskysoftware <lukeskysoftware@gmail.com>
2025-03-19 19:43:08 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,6 +2,7 @@
 session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
+    exit(); // Aggiungere exit() dopo header() per assicurarsi che lo script si interrompa
 }
 ?>
 <div class="pure-menu pure-menu-horizontal">
@@ -9,9 +10,9 @@ if (!isset($_SESSION['user_id'])) {
     <ul class="pure-menu-list">
         <li class="pure-menu-item"><a class="pure-menu-link" href="create_zone_form.html">Crea Zona</a></li>
         <li class="pure-menu-item"><a class="pure-menu-link" href="list_zones.php">Gestisci Zone</a></li> <!-- Updated to point to list_zones.php -->
-        <li class="pure-menu-item"><a class="pure-menu-link" href="address_calculate.html">Prenota Appuntamento</a></li>
+       <li class="pure-menu-item"><a class="pure-menu-link" href="address_calculate.html">Prenota Appuntamento</a></li>
         <li class="pure-menu-item"><a class="pure-menu-link" href="manage_appointments.php">Gestisci Appuntamenti</a></li>
-        
+        <li class="pure-menu-item"><a class="pure-menu-link" href="insert_appointment.php"><b></b>Inserisci Appuntamento</b></a></li>
         <li class="pure-menu-item"><a class="pure-menu-link" href="view_appointments.php">Vedi Appuntamenti</a></li>
         <li class="pure-menu-item"><a class="pure-menu-link" href="logout.php">Logout</a></li>
     </ul>
