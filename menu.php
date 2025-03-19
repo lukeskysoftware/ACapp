8788d2a8a3b54162b1b2cfe6c1940a05aa43676a
lukeskysoftware <lukeskysoftware@gmail.com>
2025-03-19 22:54:53 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,16 +5,39 @@ if (!isset($_SESSION['user_id'])) {
     exit(); // Aggiungere exit() dopo header() per assicurarsi che lo script si interrompa
 }
 ?>
-<div class="pure-menu pure-menu-horizontal">
-<nav>
-    <ul class="pure-menu-list">
-        <li class="pure-menu-item"><a class="pure-menu-link" href="create_zone_form.html">Crea Zona</a></li>
-        <li class="pure-menu-item"><a class="pure-menu-link" href="list_zones.php">Gestisci Zone</a></li> <!-- Updated to point to list_zones.php -->
-       <li class="pure-menu-item"><a class="pure-menu-link" href="address_calculate.html">Prenota Appuntamento</a></li>
-        <li class="pure-menu-item"><a class="pure-menu-link" href="manage_appointments.php">Gestisci Appuntamenti</a></li>
-        <li class="pure-menu-item"><a class="pure-menu-link" href="insert_appointment.php"><b></b>Inserisci Appuntamento</b></a></li>
-        <li class="pure-menu-item"><a class="pure-menu-link" href="view_appointments.php">Vedi Appuntamenti</a></li>
-        <li class="pure-menu-item"><a class="pure-menu-link" href="logout.php">Logout</a></li>
-    </ul>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">ACapp</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="create_zone_form.html">Crea Zona</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="list_zones.php">Gestisci Zone</a> <!-- Updated to point to list_zones.php -->
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="address_calculate.html">Prenota Appuntamento</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="manage_appointments.php">Gestisci Appuntamenti</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="insert_appointment.php">Inserisci Appuntamento</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="view_appointments.php">Vedi Appuntamenti</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
-</div>
