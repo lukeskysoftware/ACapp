bcbf67aa627fe50d4c0a366f73af715ce9dc934e
lukeskysoftware <lukeskysoftware@gmail.com>
2025-03-30 00:03:36 +0100

    Update list_zones.php

--- list_zones.php
+++ list_zones.php
@@ -2,13 +2,13 @@
 include 'db.php';
 include 'menu.php'; // Include the menu
 echo "<link rel='stylesheet' type='text/css' href='styles.css'>";
-$cssurl = "https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css";
-echo "<link rel='stylesheet' type='text/css' href='$cssurl'>";
+$bootstrapCssUrl = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css";
+echo "<link rel='stylesheet' type='text/css' href='$bootstrapCssUrl'>";
 $sql = "SELECT * FROM cp_zones";
 $result = mysqli_query($conn, $sql);
-echo "<div class='pure-g aria'>";
-echo "<h2 class='centrato centro'>Gestione delle Zone </h2> </div>";
-echo "<div class='pure-g aria'><table border='0' class='pure-table pure-table-bordered centrato aria'>;
+echo "<div class='container'>";
+echo "<h2 class='text-center my-4'>Gestione delle Zone</h2>";
+echo "<div class='table-responsive'><table class='table table-bordered text-center'>
 <tr>
 <th>Nome della Zona</th>
 <th>Indirizzo</th>
@@ -17,21 +17,22 @@ echo "<div class='pure-g aria'><table border='0' class='pure-table pure-table-bo
 
 while ($row = mysqli_fetch_assoc($result)) {
     echo "<tr>";
-    echo "<td>" . $row['name'] . "</td>";
-    echo "<td>" . $row['address'] . "</td>";
+    echo "<td>" . htmlspecialchars($row['name']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['address']) . "</td>";
     echo "<td>
-            <form action='edit_zone_form.html' method='get' style='display:inline-block;'  class='pure-form pure-form-aligned centrato centro'>
-                <input type='hidden' name='zone_id' value='" . $row['id'] . "'>
-                <button type='submit' class='modifica-btn pure-button button-small button-green'>Modifica</button>
+            <form action='edit_zone_form.php' method='get' class='d-inline-block'>
+                <input type='hidden' name='zone_id' value='" . htmlspecialchars($row['id']) . "'>
+                <button type='submit' class='btn btn-success btn-sm'>Modifica</button>
             </form>
-            <form action='delete_zone.php' method='post' style='display:inline-block;' onsubmit='return confirm(\"Sei sicuro di voler eliminare questa zona?\");'>
-                <input type='hidden' name='zone_id' value='" . $row['id'] . "'>
-                <button type='submit' class='cancella-btn pure-button button-small button-red'>Elimina</button>
+            <form action='delete_zone.php' method='post' class='d-inline-block' onsubmit='return confirm(\"Sei sicuro di voler eliminare questa zona?\");'>
+                <input type='hidden' name='zone_id' value='" . htmlspecialchars($row['id']) . "'>
+                <button type='submit' class='btn btn-danger btn-sm'>Elimina</button>
             </form>
           </td>";
     echo "</tr>";
 }
 echo "</table>";
+echo "</div>";
 echo "</div>";
 
 mysqli_close($conn);
