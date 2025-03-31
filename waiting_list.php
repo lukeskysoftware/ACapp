d52409b3dc035a6f3015baf0958b83cab78e106d
lukeskysoftware <lukeskysoftware@gmail.com>
2025-03-31 08:24:12 +0200

    Update waiting_list.php

--- waiting_list.php
+++ waiting_list.php
@@ -114,6 +114,16 @@ $patients = getWaitingPatients();
             background-color: darkred;
             color: white;
         }
+         .prenota-zona {
+            background-color:#17a2b8;
+            color: #fff;
+            
+        }
+        .prenota-zona:hover {
+           
+            color: #333;
+            
+        }
     </style>
     <script>
         function confirmDelete(patientId, patientName, patientSurname, patientPhone) {
@@ -154,7 +164,7 @@ $patients = getWaitingPatients();
                         <td><?php echo htmlspecialchars($patient['phone']); ?></td>
                         <td><?php echo htmlspecialchars($patient['created_at']); ?></td>
                         <td>
-                            <a href="combined_address_calculate_v2.php?name=<?php echo urlencode($patient['name']); ?>&surname=<?php echo urlencode($patient['surname']); ?>&phone=<?php echo urlencode($patient['phone']); ?>" class="pure-button">Inserisci Appuntamento Zona</a>
+                            <a href="combined_address_calculate_v2.php?name=<?php echo urlencode($patient['name']); ?>&surname=<?php echo urlencode($patient['surname']); ?>&phone=<?php echo urlencode($patient['phone']); ?>" class="pure-button prenota-zona">Inserisci Appuntamento Zona</a>
                             <a href="insert_appointment.php?name=<?php echo urlencode($patient['name']); ?>&surname=<?php echo urlencode($patient['surname']); ?>&phone=<?php echo urlencode($patient['phone']); ?>" class="pure-button pure-button-primary">Inserisci Appuntamento</a>
                             <form id="confirm-delete-<?php echo $patient['id']; ?>" method="POST" style="display:inline;">
                                 <input type="hidden" name="delete_patient_id" value="<?php echo $patient['id']; ?>">
@@ -189,7 +199,7 @@ $patients = getWaitingPatients();
                             <td><?php echo htmlspecialchars($patient['created_at']); ?></td>
                             <td><?php echo $patient['appointment_info']; ?></td>
                             <td>
-                                <a href="combined_address_calculate.php?name=<?php echo urlencode($patient['name']); ?>&surname=<?php echo urlencode($patient['surname']); ?>&phone=<?php echo urlencode($patient['phone']); ?>" class="pure-button">Inserisci Appuntamento Zona</a>
+                                <a href="combined_address_calculate_v2.php?name=<?php echo urlencode($patient['name']); ?>&surname=<?php echo urlencode($patient['surname']); ?>&phone=<?php echo urlencode($patient['phone']); ?>" class="pure-button prenota-zona">Inserisci Appuntamento Zona</a>
                                 <a href="insert_appointment.php?name=<?php echo urlencode($patient['name']); ?>&surname=<?php echo urlencode($patient['surname']); ?>&phone=<?php echo urlencode($patient['phone']); ?>" class="pure-button pure-button-primary">Inserisci Appuntamento</a>
                             </td>
                         </tr>
